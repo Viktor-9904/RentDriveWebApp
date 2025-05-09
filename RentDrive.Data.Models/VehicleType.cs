39fe8a29a05dd6e37e46dfdde25ad3b4d20bc947
Viktor Stanev <viktorstanev192@gmail.com>
2025-05-09 19:13:03 +0300

    Configured VehicleType using Fluent API.

---
+++ RentDrive.Data.Models/VehicleType.cs
@@ -0,0 +1,12 @@
+﻿namespace RentDrive.Data.Models
+{
+    public class VehicleType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public ICollection<Vehicle> Vehicles { get; set; }
+            = new HashSet<Vehicle>();
+        public ICollection<VehicleTypeCategory> VehicleTypeCategory { get; set; }
+            = new HashSet<VehicleTypeCategory>();
+    }
+}
---
+++ RentDrive.Data/Configuration/VehicleTypeConfiguration.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using RentDrive.Data.Models;
+using static RentDrive.Common.EntityValidationConstants.VehicleType;
+
+namespace RentDrive.Data.Configuration
+{
+    public class VehicleTypeConfiguration : IEntityTypeConfiguration<VehicleType>
+    {
+        public void Configure(EntityTypeBuilder<VehicleType> builder)
+        {
+            builder
+                .HasKey(vt => vt.Id);
+
+            builder
+                .Property(vt => vt.Name)
+                .HasMaxLength(NameMaxLength)
+                .IsRequired()
+                .HasComment("Name of the vehicle type.");
+        }
+    }
+
+    public static class VehicleTypeSeeder
+    {
+        public static IEnumerable<VehicleType> SeedVehicleTypes()
+        {
+            IEnumerable<VehicleType> vehicleTypes = new List<VehicleType>()
+            {
+                new()
+                {
+                    Id = 1,
+                    Name = "Car"
+                },
+                new()
+                {
+                    Id = 2,
+                    Name = "Truck"
+                },
+                new()
+                {
+                    Id = 3,
+                    Name = "Motorcycle"
+                },
+                new()
+                {
+                    Id = 4,
+                    Name = "Bicycle"
+                },
+                new()
+                {
+                    Id = 5,
+                    Name = "Electric Scooter"
+                },
+                new()
+                {
+                    Id = 6,
+                    Name = "All Terrain Vehicle (ATV)"
+                },
+                new()
+                {
+                    Id = 7,
+                    Name = "Camper Trailer"
+                },
+                new()
+                {
+                    Id = 8,
+                    Name = "Recreational Vehicle"
+                },
+                new()
+                {
+                    Id = 9,
+                    Name = "Limousine"
+                }
+            };
+            return vehicleTypes;
+        }
+    }
+}
