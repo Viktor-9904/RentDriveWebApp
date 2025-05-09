937ada5bb1e0f8c889c2de450c18b9236aa42a1d
Viktor Stanev <viktorstanev192@gmail.com>
2025-05-09 19:09:48 +0300

    Added Entity Validation Constants.

---
+++ RentDrive.Common/EntityValidationConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace RentDrive.Common
+{
+    public static class EntityValidationConstants
+    {
+        public static class Vehicle
+        {
+            public const int MakeMaxLength = 100;
+            public const int ModelMaxLength = 100;
+            public const int ColorMaxLength = 50;
+            public const int DescriptionMaxLength = 1500;
+        }
+        public static class VehicleTypeClass
+        {
+            public const int NameMaxLength = 50;
+            public const int DescriptionMaxLength = 1500;
+        }
+        public static class VehicleType
+        {
+            public const int NameMaxLength = 50;
+        }
+    }
+}
