f27a7fa70d8497cd1d12e16eb19e017fbe0ee448
Viktor Stanev <viktorstanev192@gmail.com>
2025-05-09 19:16:28 +0300

    Updated DbContext and implemented entity data seeding.

--- RentDrive.Data/RentDriveDbContext.cs
+++ RentDrive.Data/RentDriveDbContext.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Reflection;
+
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
 using RentDrive.Data.Models;
+using static RentDrive.Data.Configuration.VechicleSeeder;
+using static RentDrive.Data.Configuration.VehicleTypeCategorySeeder;
+using static RentDrive.Data.Configuration.VehicleTypeSeeder;
 
 namespace RentDrive.Data
 {
@@ -13,6 +19,21 @@ namespace RentDrive.Data
         }
         public RentDriveDbContext(DbContextOptions options) : base(options)
         {
+
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; } = null!;
+        public DbSet<VehicleType> VehicleTypes { get; set; } = null!;
+        public DbSet<VehicleTypeCategory> VehicleTypeClasses { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            modelBuilder.Entity<VehicleType>().HasData(SeedVehicleTypes());
+            modelBuilder.Entity<VehicleTypeCategory>().HasData(SeedVehicleTypeCategories());
+            modelBuilder.Entity<Vehicle>().HasData(SeedVehicles());
 
         }
     }
