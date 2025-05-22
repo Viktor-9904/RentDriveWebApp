4dac2c4de26d0c8f6f82daa71c03a8f73b6e5d97
Viktor Stanev <viktorstanev192@gmail.com>
2025-05-22 11:57:09 +0300

    Added Custom Logo.

--- RentDrive.Frontend/src/App.jsx
+++ RentDrive.Frontend/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
                 {/* <!-- ***** Menu Start ***** --> */}
                 <ul className="nav">
                   <li><a href="index.html" className="active">Home</a></li>
-                  <li><a href="category.html">Category</a></li>
+                  <li><a href="category.html">Categories</a></li>
                   <li><a href="listing.html">Listing</a></li>
                   <li><a href="contact.html">Contact Us</a></li>
                   <li><div className="main-white-button"><a href="#"><i className="fa fa-plus"></i> Add Your Listing</a></div></li>
