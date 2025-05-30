f133bad02275eb2e596a7823b4a57a6dee6a01bd
Viktor Stanev <viktorstanev192@gmail.com>
2025-05-30 15:10:03 +0300

    Added RecentVehicleIndexViewModel.

---
+++ RentDrive.Web.ViewModels/Vehicles/RecentVehicleIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RentDrive.Web.ViewModels.Vehicles
+{
+    public class RecentVehicleIndexViewModel
+    {
+        public string Make { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public decimal PricePerHour { get; set; }
+        public string FuelType { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+}
