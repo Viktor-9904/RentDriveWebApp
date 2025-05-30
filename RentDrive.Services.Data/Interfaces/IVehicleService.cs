e4440907c7cdc4a4a8fb387961a6a125c070aa9e
Viktor Stanev <viktorstanev192@gmail.com>
2025-05-30 15:09:30 +0300

    Added IVehicleService Interface.

---
+++ RentDrive.Services.Data/Interfaces/IVehicleService.cs
@@ -0,0 +1,10 @@
+﻿using RentDrive.Web.ViewModels;
+using RentDrive.Web.ViewModels.Vehicles;
+
+namespace RentDrive.Services.Data.Interfaces
+{
+    public interface IVehicleService
+    {
+        Task<IEnumerable<RecentVehicleIndexViewModel>> IndexGetTop3RecentVehicles();
+    }
+}
