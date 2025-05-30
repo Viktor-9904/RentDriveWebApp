d93290ca6cbdf2e125967d35d52d15b3ff62f91e
Viktor Stanev <viktorstanev192@gmail.com>
2025-05-30 15:25:03 +0300

    Added DateAdded to Vehicle Entity. Updated static Vehicle Seeder.

---
+++ RentDrive.Data/Migrations/20250530122327_addedDateAddedPropertyToVehicles.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentDrive.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedDateAddedPropertyToVehicles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateAdded",
+                table: "Vehicles",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: new Guid("6a8e2d12-04a3-4c55-8b2b-f9a0f1fd35de"),
+                column: "DateAdded",
+                value: new DateTime(2022, 7, 12, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: new Guid("fe15cde2-1a90-46d4-89f1-10fda7f11743"),
+                column: "DateAdded",
+                value: new DateTime(2023, 2, 22, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Vehicles",
+                keyColumn: "Id",
+                keyValue: new Guid("ff71fcbc-6829-47fd-81c7-d16d7c2c34b4"),
+                column: "DateAdded",
+                value: new DateTime(2024, 9, 19, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateAdded",
+                table: "Vehicles");
+        }
+    }
+}
