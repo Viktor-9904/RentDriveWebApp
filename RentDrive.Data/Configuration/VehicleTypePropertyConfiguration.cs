0012f8865eef2419d01e3c89e7cd837760cb1086
Viktor Stanev <viktorstanev192@gmail.com>
2025-06-19 16:16:21 +0300

    Added VehicleTypePropertySeeder.

--- RentDrive.Data/Configuration/VehicleTypePropertyConfiguration.cs
+++ RentDrive.Data/Configuration/VehicleTypePropertyConfiguration.cs
@@ -39,4 +39,47 @@ namespace RentDrive.Data.Configuration
                 .HasDefaultValue(UnitOfMeasurement.None);
         }
     }
+    public static class VehicleTypePropertySeeder
+    {
+        public static IEnumerable<VehicleTypeProperty> SeedVehicleTypeProperties()
+        {
+            IEnumerable<VehicleTypeProperty> vehicleTypeProperties = new List<VehicleTypeProperty>()
+            {
+                new()
+                {
+                   Id = Guid.Parse("671fbc1d-53ef-4862-9f73-974b94ac0d25"),
+                   VehicleTypeId = 1,
+                   Name = "Cubic Centimeters",
+                   ValueType = PropertyValueType.Int,
+                   UnitOfMeasurement = UnitOfMeasurement.Cc,
+                },
+                new()
+                {
+                   Id = Guid.Parse("13aaad5c-624c-4361-a863-6ff301f5c63d"),
+                   VehicleTypeId = 1,
+                   Name = "Door Count",
+                   ValueType = PropertyValueType.Int,
+                   UnitOfMeasurement = UnitOfMeasurement.None,
+                },
+                new()
+                {
+                   Id = Guid.Parse("77ab0d68-a40a-413a-9b4a-aa30716609db"),
+                   VehicleTypeId = 1,
+                   Name = "Seat Count",
+                   ValueType = PropertyValueType.Int,
+                   UnitOfMeasurement = UnitOfMeasurement.None,
+                },
+                new()
+                {
+                   Id = Guid.Parse("dd348516-a1dc-4336-b6c8-fb885b7afd0f"),
+                   VehicleTypeId = 1,
+                   Name = "Power in KiloWatts",
+                   ValueType = PropertyValueType.Double,
+                   UnitOfMeasurement = UnitOfMeasurement.kW,
+                }
+            };
+
+            return vehicleTypeProperties;
+        }
+    }
 }
