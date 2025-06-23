8ff2f0b441f34a46fe12821660a5a5ce2f662434
Viktor Stanev <viktorstanev192@gmail.com>
2025-06-23 11:50:07 +0300

    Added VehicleTypePropertyValue Service.

---
+++ RentDrive.Services.Data/Interfaces/IVehicleTypePropertyValueService.cs
@@ -0,0 +1,10 @@
+﻿using RentDrive.Data.Models;
+using RentDrive.Data.Repository.Interfaces;
+
+namespace RentDrive.Services.Data.Interfaces
+{
+    public interface IVehicleTypePropertyValueService
+    {
+        Task<IEnumerable<VehicleTypePropertyValue>> GetVehicleTypePropertyValuesByVehicleIdAsync(Guid vehicleId);
+    }
+}
---
+++ RentDrive.Services.Data/VehicleTypePropertyValueService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentDrive.Data.Models;
+using RentDrive.Data.Repository.Interfaces;
+using RentDrive.Services.Data.Interfaces;
+
+namespace RentDrive.Services.Data
+{
+    public class VehicleTypePropertyValueService : IVehicleTypePropertyValueService
+    {
+        private readonly IRepository<VehicleTypePropertyValue, Guid> vehicleTypePropertyValueRepository;
+
+        public VehicleTypePropertyValueService(IRepository<VehicleTypePropertyValue, Guid> vehicleTypePropertyValueRepository)
+        {
+            this.vehicleTypePropertyValueRepository = vehicleTypePropertyValueRepository;
+        }
+
+        public async Task<IEnumerable<VehicleTypePropertyValue>> GetVehicleTypePropertyValuesByVehicleIdAsync(Guid vehicleId)
+        {
+            List<VehicleTypePropertyValue> vehicleTypePropertyValues = await this.vehicleTypePropertyValueRepository
+                .GetAllAsQueryable()
+                .Where(vtpv => vtpv.VehicleId == vehicleId)
+                .ToListAsync();
+
+            return vehicleTypePropertyValues;
+        }
+    }
+}
