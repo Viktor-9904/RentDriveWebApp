0d3cad234ea007acb0051706dd3b84532d9f1ae2
Viktor Stanev <viktorstanev192@gmail.com>
2025-06-23 11:50:24 +0300

    Added VehicleTypePropertyValue ViewModel.

---
+++ RentDrive.Web.ViewModels/Vehicles/VehicleTypePropertyValuesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RentDrive.Web.ViewModels.Vehicles
+{
+    public class VehicleTypePropertyValuesViewModel
+    {
+        public string VehicleTypePropertyName { get; set; } = null!;
+        public string VehicleTypePropertyValue { get; set; } = null!;
+        public string UnitOfMeasurement { get; set; } = null!;
+    }
+}
