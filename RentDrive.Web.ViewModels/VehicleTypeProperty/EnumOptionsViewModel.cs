c2d67a7fbdd735b3203960faa121b5268386a4ef
Viktor Stanev <viktorstanev192@gmail.com>
2025-06-30 14:49:04 +0300

    Added EnumOptions, ValueType and UnitOfMeasurement ViewModels.

---
+++ RentDrive.Web.ViewModels/VehicleTypeProperty/EnumOptionsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RentDrive.Web.ViewModels.VehicleTypeProperty
+{
+    public class EnumOptionsViewModel
+    {
+        public IEnumerable<ValueTypeViewModel> ValueTypes { get; set; }
+            = new List<ValueTypeViewModel>();
+
+        public IEnumerable<UnitOfMeasurementViewModel> UnitOfMeasurements { get; set; }
+            = new List<UnitOfMeasurementViewModel>();
+    }
+}
---
+++ RentDrive.Web.ViewModels/VehicleTypeProperty/ValueTypeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RentDrive.Web.ViewModels.VehicleTypeProperty
+{
+    public class ValueTypeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
