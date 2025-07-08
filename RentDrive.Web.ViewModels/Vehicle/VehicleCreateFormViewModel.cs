489c922766ea537ba9a28bf218cdf7bd77ba198b
Viktor Stanev <viktorstanev192@gmail.com>
2025-07-08 15:33:17 +0300

    Added VehicleCreateFormViewModel.

---
+++ RentDrive.Web.ViewModels/Vehicle/VehicleCreateFormViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using RentDrive.Common.Enums;
+using RentDrive.Web.ViewModels.VehicleTypePropertyValue;
+
+namespace RentDrive.Web.ViewModels.Vehicle
+{
+    public class VehicleCreateFormViewModel
+    {
+        [Required]
+        public string Make { get; set; } = null!;
+        [Required]
+        public string Model { get; set; } = null!;
+        [Required]
+        public string Color { get; set; } = null!;
+        [Required]
+        public decimal PricePerDay { get; set; }
+        [Required]
+        public FuelType FuelType { get; set; }
+        [Required]
+        public DateTime DateOfProduction { get; set; }
+        [Required]
+        public int CurbWeightInKg { get; set; }
+        [Required]
+        public string Description { get; set; } = null!;
+        [Required]
+        public int VehicleTypeId { get; set; }
+        [Required]
+        public IEnumerable<CreateFormVehicleTypePropertyValueViewModel> PropertyValues { get; set; }
+            = new List<CreateFormVehicleTypePropertyValueViewModel>();
+    }
+}
---
+++ RentDrive.Web.ViewModels/VehicleTypePropertyValue/CreateFormVehicleTypePropertyValueViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentDrive.Web.ViewModels.VehicleTypePropertyValue
+{
+    public class CreateFormVehicleTypePropertyValueViewModel
+    {
+        [Required]
+        public Guid PropertyId { get; set; }
+        [Required]
+        public string Value { get; set; } = null!;
+    }
+}
