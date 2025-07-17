c168e306fed426436bce8afc60247f57d2800f54
Viktor Stanev <viktorstanev192@gmail.com>
2025-07-17 16:28:56 +0300

    Created Rental model and RentalStatus enum.

---
+++ RentDrive.Common/Enums/RentalStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace RentDrive.Common.Enums
+{
+    public enum RentalStatus
+    {
+        Active = 0,
+        Completed = 1,
+        Cancelled = 2,
+    }
+}
---
+++ RentDrive.Data.Models/Rental.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using RentDrive.Common.Enums;
+using System.Net.Http.Headers;
+
+namespace RentDrive.Data.Models
+{
+    public class Rental
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid VehicleId { get; set; }
+        public Vehicle Vehicle { get; set; } = null!;
+        public Guid RenterId { get; set; }
+        public ApplicationUser Renter { get; set; } = null!;
+        public DateTime BookedOn { get; set; } = DateTime.UtcNow;
+        public DateTime? CancelledOn { get; set; }
+        public DateTime? CompletedOn { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public RentalStatus Status { get; set; } = RentalStatus.Active;
+    }
+}
