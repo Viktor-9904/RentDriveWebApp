82b4b43bf2eee5ceecae5daa9f153acff3586537
Viktor Stanev <viktorstanev192@gmail.com>
2025-11-03 16:31:42 +0200

    Removed extra padding from “No reviews” message.

--- RentDrive.Frontend/src/components/VehicleDetails/Reviews/ReviewList.jsx
+++ RentDrive.Frontend/src/components/VehicleDetails/Reviews/ReviewList.jsx
@@ -3,17 +3,18 @@ import "./ReviewList.css"
 export default function ReviewList({ reviews }) {
     if (!reviews || reviews.length === 0) {
         return (
-            <div className="row justify-content-center mt-4">
-                <div className="col-md-8 col-lg-6">
-                    <div className="orange-box p-4">
-                        <h5 className="section-heading mb-3 text-start">User Reviews</h5>
-                        <p className="no-reviews-text mb-0">
-                            This vehicle hasn't been reviewed yet.
-                        </p>
+            <div className="review-list">
+                <div className="row justify-content-center mt-4">
+                    <div className="col-md-8 col-lg-6">
+                        <div className="orange-box p-4">
+                            <h5 className="review-section-heading mb-3">User Reviews</h5>
+                            <p className="no-reviews-text mb-0">
+                                This vehicle hasn't been reviewed yet.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-
         );
     }
 
@@ -28,7 +29,7 @@ export default function ReviewList({ reviews }) {
                                 <div className="d-flex justify-content-between align-items-center mb-2">
                                     <span className="review-username">{review.username}</span>
                                     {typeof review.starRating === 'number' && (
-                                        <ul className="rate d-flex list-unstyled mb-0 review-stars">
+                                        <ul className="d-flex list-unstyled mb-0 review-stars">
                                             {[...Array(5)].map((_, i) => {
                                                 if (review.starRating >= i + 1) {
                                                     return <li key={i}><i className="fa fa-star"></i></li>;
