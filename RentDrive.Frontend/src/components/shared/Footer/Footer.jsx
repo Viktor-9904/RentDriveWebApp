158c4b7c1e63539421ec60931546d090f36ac3f3
Viktor Stanev <viktorstanev192@gmail.com>
2025-11-05 16:48:52 +0200

    Made Footer component fully mobile responsive.

--- RentDrive.Frontend/src/components/shared/Footer/Footer.jsx
+++ RentDrive.Frontend/src/components/shared/Footer/Footer.jsx
@@ -8,61 +8,61 @@ export default function Footer() {
     const currentYear = new Date().getFullYear();
     
     return (
-            <footer>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-4">
-                            <div className="about">
-                                <div className="logo">
-                                    <img src="assets/images/black-logo.png" alt="Plot Listing" />
-                                </div>
-                                <p>Copyright © {currentYear > appPublishYear ? `${appPublishYear} - ${currentYear}` : currentYear} Rent Drive Co., Ltd. All Rights Reserved.</p>
+        <footer>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-4">
+                        <div className="about">
+                            <div className="logo">
+                                <img src="assets/images/black-logo.png" alt="Plot Listing" />
                             </div>
-                        </div>
-                        <div className="col-lg-4">
-                            <div className="helpful-links">
-                                <h4>Helpful Links</h4>
-                                <div className="row">
-                                    <div className="col-lg-6 col-sm-6">
-                                        <ul>
-                                            <li><Link to="/listing">Listing</Link></li>
-                                            <li><Link to="/contact-us">Contact Us</Link></li>
-                                        </ul>
-                                    </div>
-                                    <div className="col-lg-6">
-                                        <ul>
-                                            <li><Link to="#">About Us</Link></li>
-                                            <li><Link to="#">Privacy Policy</Link></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-4">
-                            <div className="contact-us">
-                                <h4>Contact Us</h4>
-                                <p>27th Street of New Town, Digital Villa</p>
-                                <div className="row">
-                                    <div className="col-lg-6">
-                                        <Link to="tel:0100200340">010-020-0340</Link>
-                                        <br />
-                                        <Link to="tel:0800800760">080-080-0760</Link>
-                                        <br />
-                                        <Link to="tel:09008009040">090-080-9040</Link>
-                                    </div>
-                                    <div className="col-lg-6">
-                                        <Link to="support@rentdrive.eu">support@rentdrive.eu</Link>
-                                        <br />
-                                        <Link to="https://github.com/viktor-9904" target="_blank" rel="noopener noreferrer">GitHub</Link>
-                                        <br />
-                                        <Link to="https://www.linkedin.com/in/viktor-stanev-67b55a366/" target="_blank" rel="noopener noreferrer">LinkedIn</Link>
-                                    </div>
-                                </div>
-                            </div>
-
+                            <p>Copyright © {currentYear > appPublishYear ? `${appPublishYear} - ${currentYear}` : currentYear} Rent Drive Co., Ltd. All Rights Reserved.</p>
                         </div>
                     </div>
+                    <div className="col-lg-4">
+                        <div className="helpful-links">
+                            <h4>Helpful Links</h4>
+                            <div className="row">
+                                <div className="col-lg-6 col-sm-6">
+                                    <ul>
+                                        <li><Link to="/listing">Listing</Link></li>
+                                        <li><Link to="/contact-us">Contact Us</Link></li>
+                                    </ul>
+                                </div>
+                                <div className="col-lg-6">
+                                    <ul>
+                                        <li><Link to="#">About Us</Link></li>
+                                        <li><Link to="#">Privacy Policy</Link></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="contact-us">
+                            <h4>Contact Us</h4>
+                            <p>27th Street of New Town, Digital Villa</p>
+                            <div className="row">
+                                <div className="col-lg-6">
+                                    <Link to="tel:0100200340">010-020-0340</Link>
+                                    <br />
+                                    <Link to="tel:0800800760">080-080-0760</Link>
+                                    <br />
+                                    <Link to="tel:09008009040">090-080-9040</Link>
+                                </div>
+                                <div className="col-lg-6">
+                                    <Link to="support@rentdrive.eu">support@rentdrive.eu</Link>
+                                    <br />
+                                    <Link to="https://github.com/viktor-9904" target="_blank" rel="noopener noreferrer">GitHub</Link>
+                                    <br />
+                                    <Link to="https://www.linkedin.com/in/viktor-stanev-67b55a366/" target="_blank" rel="noopener noreferrer">LinkedIn</Link>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-            </footer>
+            </div>
+        </footer>
     )
 }
